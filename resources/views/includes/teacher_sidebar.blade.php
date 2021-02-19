9b5871e4c784dafaa4749b141911244b52f3ab90
Benben Bagus <benben4567@gmail.com>
2021-02-19 17:10:30 +0700

    fix sidebar link

--- resources/views/includes/teacher_sidebar.blade.php
+++ resources/views/includes/teacher_sidebar.blade.php
@@ -1,4 +1,5 @@
-{{-- Dosen --}}
+              {{-- Dosen --}}
               <li class="menu-header">Dosen</li>
-              <li><a class="nav-link" href="#"><i class="fas fa-user"></i> <span>Data Diri</span></a></li>
-              <li><a class="nav-link" href="#"><i class="fas fa-book"></i> <span>Review PKM</span></a></li>
+              <li class="{{ set_active('teacher.profile.index') }}"><a class="nav-link" href="{{ route('teacher.profile.index') }}"><i class="fas fa-user"></i> <span>Data Diri</span></a></li>
+              <li class="{{ set_active('teacher.proposal.index') }}"><a class="nav-link" href="{{ route('teacher.proposal.index') }}"><i class="fas fa-book"></i> <span>Proposal PKM</span></a></li>
+              {{-- <li class="{{ set_active('teacher.panduan.index') }}"><a class="nav-link" href="{{ route('teacher.panduan.index') }}"><i class="fas fa-exclamation-circle"></i> <span>Panduan SIM</span></a></li> --}}
