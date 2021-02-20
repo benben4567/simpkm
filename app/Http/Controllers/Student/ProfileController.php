d02cc4849762cabd4f0067d910a20320adfaddef
Benben Bagus <benben4567@gmail.com>
2021-02-20 20:41:47 +0700

    Add update password method

--- app/Http/Controllers/Student/ProfileController.php
+++ app/Http/Controllers/Student/ProfileController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
 use App\Major;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -14,5 +16,32 @@ class ProfileController extends Controller
       $student = Auth::user()->student;
       $majors = Major::all();
       return view('pages.student.profile', compact('student', 'majors'));
+    }
+
+    public function updatePassword(Request $request)
+    {
+      $this->validate($request,[
+        'password' => 'required|min:8|confirmed'
+      ]);
+
+      $user = User::whereId($request->input('id'))->update([
+        'password' => Hash::make($request->input('password'))
+      ]);
+
+      if ($user) {
+        if ($request->ajax()) {
+          return response()->json([
+            'success' => true,
+            'msg' => 'Password berhasil diupdate'
+          ], 201);
+        }
+      } else {
+        if ($request->ajax()) {
+          return response()->json([
+            'success' => false,
+            'msg' => 'Password gagal diupdate'
+          ], 500);
+        }
+      }
     }
 }
--- app/Http/Controllers/Teacher/ProfileController.php
+++ app/Http/Controllers/Teacher/ProfileController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Teacher;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Major;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -23,6 +25,28 @@ class ProfileController extends Controller
 
     public function updatePassword(Request $request)
     {
-      # code...
+      $this->validate($request,[
+        'password' => 'required|min:8|confirmed'
+      ]);
+
+      $user = User::whereId($request->input('id'))->update([
+        'password' => Hash::make($request->input('password'))
+      ]);
+
+      if ($user) {
+        if ($request->ajax()) {
+          return response()->json([
+            'success' => true,
+            'msg' => 'Password berhasil diupdate'
+          ], 201);
+        }
+      } else {
+        if ($request->ajax()) {
+          return response()->json([
+            'success' => false,
+            'msg' => 'Password gagal diupdate'
+          ], 500);
+        }
+      }
     }
 }
