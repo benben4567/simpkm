54b382276fe30f0162103cbe458aba1012876fcd
Benben Bagus <benben4567@gmail.com>
2021-02-20 10:21:49 +0700

    initial commit

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Imports;
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class UsersImport implements ToCollection, WithHeadingRow
+{
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) {
+          $teacher = User::create([
+            'name' => $row['nama'],
+            'email' => $row['email'],
+            'password' => Hash::make($row['nidn']),
+            'role' => $row['role'],
+          ]);
+          $teacher->teacher()->create([
+            'major_id' => $row['id_prodi'],
+            'nidn' => $row['nidn'],
+            'nama' => $row['nama'],
+          ]);
+        }
+    }
+}
