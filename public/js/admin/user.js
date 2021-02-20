55c0c453fbc5c65acff4908821ba2b82b629a9dc
Benben Bagus <benben4567@gmail.com>
2021-02-20 10:22:28 +0700

    fix bug

--- public/js/admin/user.js
+++ public/js/admin/user.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
       }
   });
 
+  $('input[type="file"]').change(function(e){
+    var fileName = e.target.files[0].name;
+    $('.custom-file-label').html(fileName);
+  });
+
   var table1 = $("#table-admin").DataTable({
     dom: "tip",
     ordering: false,
@@ -27,6 +32,61 @@ $(document).ready(function () {
   $('#adminModal').on('show.bs.modal', function (e) {
     $("#form-admin").trigger("reset");
   })
+
+  $("#form-teacher-import").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "post",
+      url: "user/import",
+      data: new FormData(this),
+      dataType: 'JSON',
+      contentType: false,
+      cache: false,
+      processData: false,
+      beforeSend: function() {
+        $.LoadingOverlay("show")
+        $("*").removeClass('is-invalid')
+        $("div.invalid-feedback").find('ul').empty();
+      },
+      success: function (response) {
+        console.log(response)
+        $('#modalImport').modal("hide")
+        $('.custom-file-label').html("Choose file");
+        $.LoadingOverlay("hide")
+        if (response.success) {
+          populateTableTeacher(response.data)
+          Swal.fire(
+            "Berhasil!",
+            'User dosen sudah disimpan.',
+            'success'
+          )
+        }
+      },
+      error: function(xhr) {
+        $.LoadingOverlay('hide')
+        switch (xhr.status) {
+          case 422:
+            let errors = xhr.responseJSON.errors
+            Swal.fire(
+              'Error!',
+              xhr.responseJSON.message,
+              'error'
+            )
+
+            $.each(errors, function(key, value) {
+              $("input[name="+ key +"]").addClass("is-invalid")
+              $.each(errors[key], function(ke, val) {
+                $('<li>'+val+"</li>").appendTo($("div[name=msg_"+ key +"]").find('ul'));
+              })
+            })
+            break;
+
+          default:
+            break;
+        }
+      }
+    });
+  });
 
   $("#form-admin").submit(function (e) {
     e.preventDefault();
@@ -289,6 +349,61 @@ $(document).ready(function () {
     $('#table-student').DataTable().clear().destroy();
     // re-initiate filled datatable
     window.table = $('#table-student').DataTable({
+      stateSave: true,
+      dom: "tip",
+      data: data,
+      responsive: true,
+      ordering: false,
+      "columnDefs": [
+        { "className": "text-center text-nowrap", "targets": "_all" },
+        {
+          "targets": 0,
+          "data": null,
+          "render": function (data, type, row, meta) {
+            return meta.row + meta.settings._iDisplayStart + 1;
+          }
+        },
+        {
+          "targets": 1,
+          "data": null,
+          "render": function (data, type, row, meta) {
+            return row.name;
+          }
+        },
+        {
+          "targets":2,
+          "data": null,
+          "render": function (data, type, row, meta) {
+            return row.email
+          }
+        },
+        {
+          "targets" : 3,
+          "data" : null,
+          "render": function (data, type, row, meta) {
+            if (row.status == "aktif") {
+              return `<span class="badge badge-success">AKTIF</span>`;
+            } else {
+              return `<span class="badge badge-danger">NONAKTIF</span>`;
+            }
+          }
+        },
+        {
+          "targets": 4,
+          "data": null,
+          "render": function ( data, type, row, meta ) {
+            return `<button type="button" class="btn btn-sm btn-icon btn-primary student-edit" data-id="${row.id}"><i class="fas fa-eye"></i></button>`
+          }
+        },
+      ]
+    });
+  }
+
+  function populateTableTeacher(data) {
+    // destroy datatable
+    $('#table-teacher').DataTable().clear().destroy();
+    // re-initiate filled datatable
+    window.table = $('#table-teacher').DataTable({
       stateSave: true,
       dom: "tip",
       data: data,
