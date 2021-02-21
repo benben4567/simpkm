628c1b437988ec22241bbe017f86b85a13da515c
Benben Bagus <benben4567@gmail.com>
2021-02-21 14:49:14 +0700

    Fix date format

--- public/js/dosen/profile.js
+++ public/js/dosen/profile.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
       }
   });
 
-  $('.datepicker').datepicker()
+  $('.datepicker').datepicker({
+    format: "yyyy-mm-dd"
+  })
 
   // update password
   $('#update-password').submit(function (e) {
@@ -55,6 +57,60 @@ $(document).ready(function () {
               'error'
             )
             break;
+          default:
+            break;
+        }
+      }
+    });
+  });
+
+  $("#form-teacher").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "put",
+      url: "profile",
+      data: $(this).serialize(),
+      dataType: "JSON",
+      beforeSend: function() {
+        $.LoadingOverlay("show")
+        $("*").removeClass('is-invalid')
+        $("div.invalid-feedback").find('ul').empty();
+      },
+      success: function (response) {
+        $.LoadingOverlay("hide")
+        if (response.success) {
+          Swal.fire({
+            title: 'Berhasil!',
+            text: response.msg,
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              location.reload()
+            }
+          })
+        }
+      },
+      error: function(xhr) {
+        $.LoadingOverlay('hide')
+        switch (xhr.status) {
+          case 422:
+            let errors = xhr.responseJSON.errors
+            Swal.fire(
+              'Error!',
+              xhr.responseJSON.message,
+              'error'
+            )
+
+            $.each(errors, function(key, value) {
+              $("[name="+ key +"]").addClass("is-invalid")
+              $.each(errors[key], function(ke, val) {
+                $('<li>'+val+"</li>").appendTo($("div[name=msg_"+ key +"]").find('ul'));
+              })
+            })
+            break;
+
           default:
             break;
         }
