e96719959eeb2e0e6d080807cb3667ad4ead7d25
Benben Bagus <benben4567@gmail.com>
2021-02-22 16:46:04 +0700

    Change js link

--- resources/views/auth/login2.blade.php
+++ resources/views/auth/login2.blade.php
@@ -92,10 +92,10 @@
   </div>
 
   <!-- General JS Scripts -->
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="{{ asset('vendor/jquery/jquery-3.5.1.min.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+  <script src="{{ asset('vendor/jquery-nicescroll/jquery.nicescroll.min.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
   <script src="{{ asset('vendor/stisla/js/stisla.js')}}"></script>
 
