a14946e7cb2fb7c94121d4cb3da018736178a86a
Benben Bagus <benben4567@gmail.com>
2021-02-23 20:46:01 +0700

    Add alert periode ditutup

--- resources/views/pages/student/proposal_create.blade.php
+++ resources/views/pages/student/proposal_create.blade.php
@@ -8,6 +8,7 @@
       <div class="section-body">
         <div class="row">
           <div class="col-12">
+          @if($periode->status == 'buka')
             <div class="card">
               <form action="{{ route('proposal.store') }}" method="post" autocomplete="off" enctype="multipart/form-data">
               <div class="card-body">
@@ -99,6 +100,25 @@
               </div>
               </form>
             </div>
+          @else
+            @if(date("Y") == $periode->tahun)
+              <div class="alert alert-warning alert-has-icon">
+                <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                <div class="alert-body">
+                  <div class="alert-title">Perhatian</div>
+                  <h5> Usulan proposal PKM {{ $periode->tahun }} sudah ditutup. </h5>
+                </div>
+              </div>
+            @else
+              <div class="alert alert-info alert-has-icon">
+                <div class="alert-icon"><i class="far fa-lightbulb"></i></div>
+                <div class="alert-body">
+                  <div class="alert-title">Informasi</div>
+                  <h5> Usulan proposal PKM Tahun {{ date("Y") }}  belum dibuka. </h5>
+                </div>
+              </div>
+            @endif
+          @endif
           </div>
         </div>
       </div>
