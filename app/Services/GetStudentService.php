bbb6f2613299e561ea079e59a81492fb5706ea4e
Benben Bagus P <benben4567@gmail.com>
2024-02-01 01:30:14 +0700

    fix: add siakad api mahasiswa

--- app/Services/GetStudentService.php
+++ app/Services/GetStudentService.php
@@ -7,10 +7,13 @@ class GetStudentService
 {
     public static function checkNim($angkatan, $nim)
     {
-        
-        $url = config('services.neosiakad.url') . '/api/mahasiswa/get-nim/' . $nim;
-        
-        $response = Http::withToken(config('services.neosiakad.token'))->get($url);
+        if($angkatan >= 2023) {
+            $url = config('services.neosiakad.url') . '/api/mahasiswa/get-nim/' . $nim;
+            $response = Http::withToken(config('services.neosiakad.token'))->get($url);
+        } else {
+            $url = config('services.siakad.url') . '/api/mahasiswa/mhsByNim?nim=' . $nim;
+            $response = Http::get($url);
+        }
         
         if ($response->status() != 200) {
             return false;
