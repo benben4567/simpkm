2487eba5a392a8991e409b1b9e2883f8c6a2767f
benben4567 <benben4567@gmail.com>
2025-06-05 10:56:57 +0700

    feat: add file_path and file_url to Proposal and Review models

--- app/Models/Proposal.php
+++ app/Models/Proposal.php
@@ -8,50 +8,50 @@ use Illuminate\Database\Eloquent\Model;
 class Proposal extends Model
 {
     use Uuids;
-    protected $fillable = ['period_id', 'skema', 'judul', 'status', 'file'];
+    protected $fillable = ['period_id', 'skema', 'judul', 'status', 'file', 'file_url', 'file_path'];
 
     public function period()
     {
-      return $this->belongsTo(Period::class);
+        return $this->belongsTo(Period::class);
     }
 
     public function students()
     {
-      return $this->belongsToMany(Student::class)->withPivot('jabatan')->withTimestamps();
+        return $this->belongsToMany(Student::class)->withPivot('jabatan')->withTimestamps();
     }
 
     public function teachers()
     {
-      return $this->belongsToMany(Teacher::class)->withPivot('jabatan')->withTimestamps();
+        return $this->belongsToMany(Teacher::class)->withPivot('jabatan')->withTimestamps();
     }
 
     public function pembimbing()
     {
-      return $this->belongsToMany(Teacher::class)->wherePivot('jabatan', 'Pembimbing');
+        return $this->belongsToMany(Teacher::class)->wherePivot('jabatan', 'Pembimbing');
     }
 
     public function ketua()
     {
-      return $this->belongsToMany(Student::class)->wherePivot('jabatan', 'Ketua');
+        return $this->belongsToMany(Student::class)->wherePivot('jabatan', 'Ketua');
     }
 
     public function anggota()
     {
-      return $this->belongsToMany(Student::class)->wherePivot('jabatan', 'Anggota');
+        return $this->belongsToMany(Student::class)->wherePivot('jabatan', 'Anggota');
     }
 
     public function reviewer()
     {
-      return $this->belongsToMany(Teacher::class)->wherePivot('jabatan', 'reviewer');
+        return $this->belongsToMany(Teacher::class)->wherePivot('jabatan', 'reviewer');
     }
 
     public function reviews()
     {
-      return $this->hasMany(Review::class);
+        return $this->hasMany(Review::class);
     }
 
     public function scopeLolos($query)
     {
-      return $query->where('status', '=', 'lolos');
+        return $query->where('status', '=', 'lolos');
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    protected $fillable = ['user_id', 'type', 'description', 'file', 'acc'];
+    protected $fillable = ['user_id', 'type', 'description', 'file', 'file_url', 'file_path', 'acc'];
 
     public function proposal()
     {
-      return $this->belongsTo(Proposal::class);
+        return $this->belongsTo(Proposal::class);
     }
 
     public function user()
     {
-      return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 }
